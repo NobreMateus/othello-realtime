d58256904238a369dbf3193767dfff2e19be37e2
Mateus Nobre <mateusnobref@gmail.com>
2021-04-08 21:04:07 -0300

    Logica de entrar em uma sala criada

--- src/components/RoomsMenu/RoomsMenu.jsx
+++ src/components/RoomsMenu/RoomsMenu.jsx
@@ -1,16 +1,41 @@
-import React, {useContext} from 'react'
+import React, { useContext, useState, useEffect} from 'react'
 import { ConnectionContext } from '../../providers/ConnectionProvider'
 import RoomsMenuPresenter from './RoomsMenuPresenter'
 
 function RoomsMenu() {
     
-    const { connectionManager } = useContext(ConnectionContext) 
-    
-    const enterRoom = (roomName) => {
-        connectionManager.configRoom(roomName)
+    const { connectionManager } = useContext(ConnectionContext)
+    const [rooms, setRooms] = useState([
+        {id: 1, name: "first room", amountPlayers: 1, selected: false },
+        {id: 2, name: "second room", amountPlayers: 1, selected: false },
+        {id: 3, name: "third room", amountPlayers: 1, selected: false },
+        {id: 4, name: "fourth room", amountPlayers: 1, selected: false },
+        {id: 5, name: "fifth room", amountPlayers: 1, selected: false },
+        {id: 6, name: "sixth room", amountPlayers: 1, selected: false },
+    ])
+    const [selectedRoom, setSelectedRoom] = useState(undefined)
+
+    useEffect(()=>{
+        for(const room of rooms) {
+            if (room.selected) setSelectedRoom(room) 
+        }
+    }, [rooms])
+
+    const selectRoom = (roomId) => {
+        const newRoomsState = rooms.map(room => {
+            return {
+                ...room,
+                selected: roomId === room.id ? true : false
+            }
+        })
+        setRooms(newRoomsState)
     }
 
-    return <RoomsMenuPresenter enterRoom={enterRoom}/>
+    const enterRoom = () => {
+        connectionManager.configRoom(selectedRoom.name)
+    }
+
+    return <RoomsMenuPresenter enterRoom={enterRoom} rooms={rooms} selectRoom={selectRoom}/>
 
 }
 
--- src/components/RoomsMenu/RoomsMenuPresenter.jsx
+++ src/components/RoomsMenu/RoomsMenuPresenter.jsx
@@ -6,10 +6,19 @@ import { Link } from 'react-router-dom'
 export default function RoomsMenuPresenter(props){
     return <div className={styles["container"]}>
         <div className={styles["rooms-area"]}>
-
+            <div className={styles["rooms-header"]}>
+                <div>Nome da Sala</div>
+                <div>Q. de Jogadores</div>
+            </div>
+            {props.rooms.map( (room, num) => {
+                return <div key={`roomLine${num}`} className={styles["line-room-container"]} style={{backgroundColor: room.selected?"#FFC75F":undefined }} onClick={()=>{props.selectRoom(room.id)}}>
+                    <div className={styles["line-room-name"]}>{room.name}</div>
+                    <div className={styles["line-room-amount"]}>{room.amountPlayers}</div>
+                </div>
+            })}
         </div>
         <div className={styles["buttons-area"]}>
-            <Link to="game" style={{width:"calc(50% - 8px)"}} onClick={()=>props.enterRoom("first room")} >
+            <Link to="game" style={{width:"calc(50% - 8px)"}} onClick={()=>props.enterRoom()} >
                 <PrimaryButon style={{width:"100%"}} text="Iniciar Jogo"/>
             </Link>
             <PrimaryButon style={{width:"calc(50% - 8px)"}} text="Criar Sala"/>
